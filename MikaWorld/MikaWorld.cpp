c200c9e386204b8e43ed35661f6ddc974a9f906b
Alexander Barrentine <alexander_barrentine@yahoo.com>
2017-07-11 22:56:12 -0500

    Basic shooting added

--- MikaWorld/MikaWorld.cpp
+++ MikaWorld/MikaWorld.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include "MikaWorld.h"
 
 using namespace irr;
 
@@ -21,55 +22,174 @@ enum
 	IDFlag_IsPickable = 1 << 0,
 };
 
-class MyEventReceiver : public IEventReceiver
+class EventReceiver : public IEventReceiver
 {
+public:
+	enum Bits
+	{
+		None = 0,
+		LeftMouse = 1,
+		Escape = 1 << 1,
+	};
+
+protected:
+	int mCurrent;
+	int mPrevious;
+
 public:
 	// This is the one method that we have to implement
 	virtual bool OnEvent(const SEvent& event)
 	{
-		// Remember whether each key is down or up
 		if (event.EventType == irr::EET_KEY_INPUT_EVENT)
-			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+		{
+			if (event.KeyInput.Key == KEY_ESCAPE)
+			{
+				if (event.KeyInput.PressedDown)
+					mCurrent |= Bits::Escape;
+			}
+		}
+		else if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+		{
+
+			if (event.MouseInput.Event == EMIE_LMOUSE_PRESSED_DOWN)
+			{
+				mCurrent |= Bits::LeftMouse;
+			}
+		}
 
 		return false;
 	}
 
-	// This is used to check whether a key is being held down
-	virtual bool IsKeyDown(EKEY_CODE keyCode) const
+	void Update()
 	{
-		return KeyIsDown[keyCode];
+		mPrevious = mCurrent;
+		mCurrent = Bits::None;
 	}
 
-	MyEventReceiver()
-		: KeyIsDown{}
+	bool CheckSwitchEdge(Bits bits) const
 	{
+		int masked = (mCurrent & bits);
+		return masked != 0 && masked != (mPrevious & bits);
 	}
 
-private:
-	// We use this array to store the current state of each key
-	bool KeyIsDown[KEY_KEY_CODES_COUNT];
+	bool CheckEscape() const
+	{
+		return CheckSwitchEdge(Bits::Escape);
+	}
+
+	bool CheckLeftMouse() const
+	{
+		return CheckSwitchEdge(Bits::LeftMouse);
+	}
+
+	EventReceiver()
+		: mCurrent(Bits::None)
+		, mPrevious(Bits::None)
+	{
+	}
 };
+
+void MikaWorld::Fire()
+{
+	scene::ISceneManager* sm = mVideoDevice->getSceneManager();
+	scene::ICameraSceneNode* camera = sm->getActiveCamera();
+
+	if (!camera || !mTriangleSelector)
+		return;
+
+	ParticleImpact imp;
+	imp.mWhen = 0;
+
+	// get line of camera
+
+	core::vector3df start = camera->getPosition();
+	core::vector3df end = (camera->getTarget() - start);
+	end.normalize();
+	start += end*8.0f;
+	end = start + (end * camera->getFarValue());
+
+	core::triangle3df triangle;
+
+	core::line3d<f32> line(start, end);
+
+	// get intersection point with map
+	scene::ISceneNode* hitNode;
+	core::vector3df collisionPoint(end);
+	if (sm->getSceneCollisionManager()->getCollisionPoint(
+		line, mTriangleSelector, collisionPoint, triangle, hitNode))
+	{
+		// collides with wall
+		core::vector3df out = triangle.getNormal();
+		out.setLength(0.03f);
+
+		imp.mWhen = 1;
+		imp.mOutVector = out;
+		imp.mPos = collisionPoint;
+	}
+
+	// create fire ball
+	scene::ISceneNode* node = 0;
+	node = sm->addBillboardSceneNode(0,
+		core::dimension2d<f32>(25, 25), start);
+
+	node->setMaterialFlag(video::EMF_LIGHTING, false);
+	node->setMaterialTexture(0, mVideoDevice->getVideoDriver()->getTexture("../Asset/fireball.bmp"));
+	node->setMaterialType(video::EMT_TRANSPARENT_ADD_COLOR);
+
+	f32 length = (f32)(end - start).getLength();
+	const f32 speed = 0.6f;
+	u32 time = (u32)(length / speed);
+
+	scene::ISceneNodeAnimator* anim = nullptr;
+
+	// set flight line
+
+	anim = sm->createFlyStraightAnimator(start, end, time);
+	node->addAnimator(anim);
+	anim->drop();
+
+	anim = sm->createDeleteAnimator(time);
+	node->addAnimator(anim);
+	anim->drop();
+
+	if (imp.mWhen)
+	{
+		// create impact note
+		imp.mWhen = mVideoDevice->getTimer()->getTime() + (time - 100);
+		//Impacts.push_back(imp);
+	}
+}
 
 #define ARRAY_SIZE(arr) (sizeof(arr)/sizeof(arr[0]))
 
 int main()
 {
-	MyEventReceiver eventReceiver;
+	MikaWorld world;
+	world.Run();
+}
+
+void MikaWorld::Run()
+{
+	EventReceiver eventReceiver;
 
 	// ask user for driver
-	IrrlichtDevice *device = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 16, false, false, true, &eventReceiver);
-	if (!device)
-		return -1;
+	mVideoDevice = createDevice(video::EDT_OPENGL, core::dimension2d<u32>(640, 480), 16, false, false, true, &eventReceiver);
+	if (!mVideoDevice)
+		return;
 
-	video::IVideoDriver* driver = device->getVideoDriver();
-	scene::ISceneManager* smgr = device->getSceneManager();
+	video::IVideoDriver* driver = mVideoDevice->getVideoDriver();
+	scene::ISceneManager* smgr = mVideoDevice->getSceneManager();
 
-	device->getFileSystem()->addFileArchive("../Asset/map-20kdm2.pk3");
+	mVideoDevice->getFileSystem()->addFileArchive("../Asset/map-20kdm2.pk3");
 	scene::IAnimatedMesh* mesh = smgr->getMesh("20kdm2.bsp");
 	scene::IMeshSceneNode* node = nullptr;
 
-	if (mesh)
-		node = smgr->addOctreeSceneNode(mesh->getMesh(0), nullptr, IDFlag_IsPickable);
+	if (!mesh)
+		return;
+
+	node = smgr->addOctreeSceneNode(mesh->getMesh(0), nullptr, IDFlag_IsPickable);
+	if (!node)
+		return;
 
 	SKeyMap cameraKeys[] = {
 		SKeyMap(EKA_MOVE_FORWARD, KEY_KEY_W),
@@ -79,47 +199,45 @@ int main()
 	};
 	
 	scene::ICameraSceneNode* camera = smgr->addCameraSceneNodeFPS(nullptr, 100.0f, 0.3f, ID_IsNotPickable, cameraKeys, ARRAY_SIZE(cameraKeys), true, 0.3f);
+	if (!camera)
+		return;
+
 	camera->setPosition(core::vector3df(50, 50, -60));
 	camera->setTarget(core::vector3df(-70, 30, -60));
 
-	if (node)
-	{
-		node->setPosition(core::vector3df(-1350, -130, -1400));
+	node->setPosition(core::vector3df(-1350, -130, -1400));
 
-		scene::ITriangleSelector* selector = nullptr;
+	mTriangleSelector = smgr->createOctreeTriangleSelector(node->getMesh(), node, 128);
+	if (!mTriangleSelector)
+		return;
 
-		if (node)
-		{
-			selector = smgr->createOctreeTriangleSelector(node->getMesh(), node, 128);
-			node->setTriangleSelector(selector);
-
-			if (selector)
-			{
-				core::vector3df ellipsoidRadius(30.0f, 50.0f, 30.0f);
-				core::vector3df gravity(0, -10.0f, 0);
-				core::vector3df ellipsoidTranslation(0.0f, 30.0f, 0.0f);
-				scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(
-					selector, camera, ellipsoidRadius, gravity, ellipsoidTranslation);
-				selector->drop(); // As soon as we're done with the selector, drop it.
-				camera->addAnimator(anim);
-				anim->drop();  // And likewise, drop the animator when we're done referring to it.
-			}
-		}
-	}
+	node->setTriangleSelector(mTriangleSelector);
+	core::vector3df ellipsoidRadius(30.0f, 50.0f, 30.0f);
+	core::vector3df gravity(0, -10.0f, 0);
+	core::vector3df ellipsoidTranslation(0.0f, 30.0f, 0.0f);
+	scene::ISceneNodeAnimator* anim = smgr->createCollisionResponseAnimator(
+		mTriangleSelector, camera, ellipsoidRadius, gravity, ellipsoidTranslation);
+	camera->addAnimator(anim);
+	anim->drop();  // And likewise, drop the animator when we're done referring to it.
 
 	// disable mouse cursor
-	device->getCursorControl()->setVisible(false);
+	mVideoDevice->getCursorControl()->setVisible(false);
 
 	s32 lastFPS = -1;
 
-	while (device->run())
+	while (mVideoDevice->run())
 	{
-		if (eventReceiver.IsKeyDown(irr::KEY_ESCAPE))
+		if (eventReceiver.CheckEscape())
 			break;
 
-		if (device->isWindowActive())
+		if (mVideoDevice->isWindowActive())
 		{
 			driver->beginScene(true, true, 0);
+
+			if (eventReceiver.CheckLeftMouse())
+			{
+				Fire();
+			}
 
 			smgr->drawAll();
 
@@ -133,13 +251,11 @@ int main()
 				str += fps;
 				str += " FPS]";
 
-				device->setWindowCaption(str.c_str());
+				mVideoDevice->setWindowCaption(str.c_str());
 				lastFPS = fps;
 			}
 		}
+
+		eventReceiver.Update();
 	}
-
-	device->drop();
-
-	return 0;
 }
